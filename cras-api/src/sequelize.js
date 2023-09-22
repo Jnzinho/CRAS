c6d1c574d54020e34535b112481b6cacaf2d8976
Jnzinho <joaovitorjn@unochapeco.edu.br>
2023-09-21 22:39:15 -0300

    Mudanças sequelize.sync

--- cras-api/src/sequelize.js
+++ cras-api/src/sequelize.js
@@ -6,6 +6,9 @@ const sequelize = new Sequelize('cras', 'jnzinho', 'admin', {
   dialect: 'postgres',
 });
 
-sequelize.sync({ force: true }).then(() => console.log('DB Synced')).catch((err) => console.error(err));
+// caso precisar apagar a database, descomente isso
+// sequelize.sync({ force: true }).then(() => console.log('DB Synced')).catch((err) => console.error(err));
+
+sequelize.sync().then(() => console.log('DB Synced')).catch((err) => console.error(err));
 
 export default sequelize;
